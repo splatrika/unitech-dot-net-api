0284baba61a4f6d63dcd9205b355b959a27bf186
splatrika <tseflutty@yandex.ru>
2023-02-04 17:03:57 +0300

    Add get first year method tests

--- EasyUnitech.NetApi.Tests/UserServiceTests.cs
+++ EasyUnitech.NetApi.Tests/UserServiceTests.cs
@@ -15,8 +15,9 @@ public class UserServiceTests
 	public const string FakeBirthDay = "11.12.2000";
 	public DateTime FakeBirthDateTime = new DateTime(2000, 12, 11);
 	public const string FakePhoto = "/static/photos/ivan@12.png";
+	public const int FakeFirstYear = 2015;
 
-    public const string FakeUserPage = @$"
+	public readonly string StudentFakeUserPage = @$"
 <html>
 	<head>
 		<title>Fake user page</title>
@@ -49,16 +50,70 @@ public class UserServiceTests
 				<svg viewBox=""0 0 36 36"" class=""rating-chart""><path class=""circle"" stroke=""#ffdf00"" stroke-dasharray=""54.6, 100"" d=""M18 2 a 15.9155 15.9155 0 0 0 0 31.831 a 15.9155 15.9155 0 0 0 0 -31.831""></path></svg>
 			</div>
 		</div>
+		<div class=""userpage_block_wrap"">
+			<div class=""userpage_block_title"">
+				Корпоративные данные
+			</div>
+			<div class=""info""><strong>Институт:</strong> Информационных систем и программирования</div>
+			<div class=""info""><strong>Специальность / направление подготовки:</strong> Программирование в компьютерных системах</div>
+			<div class=""info""><strong>Профиль / специализация / направленность:</strong> Нет</div>
+			<div class=""info""><strong>Группа:</strong> П0-00</div>
+			<div class=""info""><strong>Год поступления:</strong> {FakeFirstYear}</div>
+			<div class=""info""><strong>Период обучения:</strong> 3 года 10 мес.</div>
+		</div>
+	</body>
+</html>";
+
+    public readonly string TeacherFakeUserPage = @$"
+<html>
+	<head>
+		<title>Fake user page</title>
+	</head>
+	<body>
+		<div class=""userpage_block_wrap"">
+			<div class=""userpage_block_title"">
+				Личные данные
+				<div class=""fl_right"">
+					(Это вы)
+				</div>	
+			</div>
+			<div class=""info""><strong>Фамилия:</strong> {FakeLastName}</div>
+			<div class=""info""><strong>Имя:</strong> {FakeFirstName}</div>
+			<div class=""info""><strong>Отчество:</strong> {FakePatronymic}</div>
+			<div class=""info"">
+				<strong>Дата рождения:</strong> {FakeBirthDay}		
+			</div>
+			<div class=""clearer""></div>	
+		</div>
+		<div class=""userpage_block_wrap"">
+			<div class=""userpage_block_title"">
+				Контакты
+			</div>
+			<div class=""info""><strong>Адрес электронной почты:</strong> example@example.example</div>
+		</div>
+		<div class=""user_rating"">			
+			<div class=""users_avatar_wrap"" onclick=""showModalImg('/files/upload/pages/image/stock_people.png')"" style=""background: url({FakePhoto}) top left no-repeat;"">
+				<h2 id=""user_page_rating_total"" data-toggle=""tooltip"" title="""" data-original-title=""Итоговый рейтинг по курсу в университете"">54.6%</h2>				
+				<svg viewBox=""0 0 36 36"" class=""rating-chart""><path class=""circle"" stroke=""#ffdf00"" stroke-dasharray=""54.6, 100"" d=""M18 2 a 15.9155 15.9155 0 0 0 0 31.831 a 15.9155 15.9155 0 0 0 0 -31.831""></path></svg>
+			</div>
+		</div>
+		<div class=""userpage_block_wrap"">
+			<div class=""userpage_block_title"">
+				Корпоративные данные
+			</div>
+			<div class=""info""><strong>Структурное подразделение:</strong> Колледж</div>
+			<div class=""info""><strong>Должность:</strong> Препод с:</div>
+		</div>
 	</body>
 </html>";
 
 
-	[Fact]
-	public async void GetUser()
+    [Fact]
+	public async void GetUser_ServerReturnsStudentUserPage_ParsedSuccefuly()
 	{
 		var httpServiceMock = new Mock<IHttpService>();
 		httpServiceMock.Setup(x => x.GetAsync($"{HttpConstants.Host}/user"))
-			.Returns(Task.FromResult(FakeUserPage));
+			.Returns(Task.FromResult(StudentFakeUserPage));
 
 		var loggerMock = new Mock<ILogger<UserService>>();
 
@@ -70,6 +125,43 @@ public class UserServiceTests
 		Assert.Equal(FakePatronymic, user.Patronymic);
 		Assert.Equal(FakeBirthDateTime, user.Bithday);
 		Assert.Equal(FakePhoto, user.Photo);
+	}
+
+
+	[Fact]
+	public async void TryGetFirstYear_ServerReturnsStudentUserPage_ParsedSuccefuly()
+	{
+		var httpServiceMock = new Mock<IHttpService>();
+		httpServiceMock.Setup(x => x.GetAsync($"{HttpConstants.Host}/user"))
+			.Returns(Task.FromResult(StudentFakeUserPage));
+
+		var loggerMock = new Mock<ILogger<UserService>>();
+
+		var service = new UserService(httpServiceMock.Object, loggerMock.Object);
+
+		var firstYear = 0;
+		var ok = await service.TryGetFirstYearAsync(x => firstYear = x);
+
+		Assert.True(ok);
+		Assert.Equal(FakeFirstYear, firstYear);
+	}
+
+
+	[Fact]
+	public async void TryGetFirstYear_ServerReturnsTeacherUserPage_DidNotParsed()
+	{
+        var httpServiceMock = new Mock<IHttpService>();
+        httpServiceMock.Setup(x => x.GetAsync($"{HttpConstants.Host}/user"))
+            .Returns(Task.FromResult(TeacherFakeUserPage));
+
+        var loggerMock = new Mock<ILogger<UserService>>();
+
+        var service = new UserService(httpServiceMock.Object, loggerMock.Object);
+
+        var firstYear = 0;
+        var ok = await service.TryGetFirstYearAsync(x => firstYear = x);
+
+        Assert.False(ok);
+        Assert.Equal(0, firstYear);
     }
 }
-
